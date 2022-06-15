3b01652184606bd38a8bc3d66a67ea3507754a3c
Daniel M. Lofaro <dan@danlofaro.com>
2022-06-15 18:48:56 -0400

    typing japanese now

--- linux/do_type.py
+++ linux/do_type.py
@@ -1,7 +1,34 @@
+# -*- coding: utf-8 -*-
+import codecs
 import pyautogui as pa
 import time as t
 import sys
 
+
 t.sleep(5)
-pa.typewrite(str(sys.argv))
-pa.typewrite("robots are awesome")
+
+def type_unicode2(word):
+    for char in word:
+        num = hex(ord(char))
+        pa.hotkey('ctrl', 'shift', 'u')
+        for n in num:
+            pa.typewrite(n)
+        pa.typewrite('\n')
+
+def type_unicode(word):
+    for c in word:
+        c = '%04x' % ord(c)
+        pa.keyDown('optionleft')
+        pa.typewrite(c)
+        pa.keyUp('optionleft')
+
+#pa.typewrite(str(sys.argv))
+#pa.typewrite("robots are awesome")
+jap1 = "こんにちは"
+jap2 = "こんにち"
+print(jap1 == jap1)
+print(jap1 == jap2)
+type_unicode2(jap1)
+pa.press('enter')
+#theOut = codecs.open(jap1,'r','utf-8')
+#pa.typewrite("こんにちは")
---
+++ linux/test_type_input.sh
@@ -0,0 +1,2 @@
+sleep 5
+python3 do_type.py dan #こんにちは

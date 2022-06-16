3177e7a8340116f120379c5e7f261287a4576467
Daniel M. Lofaro <dan@danlofaro.com>
2022-06-16 15:04:35 -0400

    looks like it is working with a random response with 20 percent being love related

---
+++ linux/do_response.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import codecs
+import time as t
+import sys
+import bot_response_lib as brl
+import random as r
+
+
+love     = brl.all_love
+non_love = brl.all_non_love
+
+
+#jap1 = "こんにちは"
+#jap2 = "こんにち"
+#print(jap1 == jap1)
+#print(jap1 == jap2)
+
+## Japanese argument equality testing working
+#try:
+#    print(sys.argv[1] == sys.argv[2])
+#except:
+#    print("not enough inputs")
+
+try:
+    jap1 = sys.argv[1]
+except:
+    exit(1)
+
+rlove = r.random()
+
+buff = ''
+val = None
+if (rlove > 0.8):
+    val = love
+else:
+    val = non_love
+
+
+i   = r.randint(0,len(val)-1)
+ii  = r.randint(0,len(val[i])-1)
+val1 = val[i]
+val2 = val1[ii]
+buff = val2
+
+print(buff)
--- linux/ocr_screen.sh
+++ linux/ocr_screen.sh
@@ -2,14 +2,22 @@ cp djap.png.cropped djap.png.cropped.png
 convert -resize 300% djap.png.cropped.png djap.png.cropped.300.png
 tesseract djap.png.cropped.300.png  jap_ocr -l jpn
 #THE_WORDS=$(< jap_ocr.txt)
-THE_WORDS=$(python3 read_jap.py)
+THE_WORDS_READ=$(python3 read_jap.py)
+THE_WORDS_REPLY=$(python3 do_response.py $THE_WORDS_READ)
+echo '----------'
+echo '---Read---'
+echo '----------'
+echo $THE_WORDS_READ
 echo '----------'
 echo '----------'
 echo '----------'
-echo $THE_WORDS
+echo '----------'
+echo '---Reply--'
+echo '----------'
+echo $THE_WORDS_REPLY
 echo '----------'
 echo '----------'
 echo '----------'
 #sleep 5
-python3 do_type.py $THE_WORDS
+python3 do_type.py $THE_WORDS_REPLY
 cat jap_ocr.txt

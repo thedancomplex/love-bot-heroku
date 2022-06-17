14d44ba784b936ffc3e8620031e91512e6bbee22
Daniel M. Lofaro <dan@danlofaro.com>
2022-06-17 16:44:49 -0400

    added conversation file tracking in english and in japansese, also added those files to be tracked.  this includes the metrics, time stamp in EST and phrase

--- linux/ocr_screen.sh
+++ linux/ocr_screen.sh
@@ -1,14 +1,66 @@
+BOT_NAME_ELENA='Hikari'
+BOT_NAME_DAN='Love-Bot'
+CONVERSATION_FILE='conversation'
+CONVERSATION_FILE_ENGLISH=$CONVERSATION_FILE.english
+
 cp djap.png.cropped djap.png.cropped.png
 convert -resize 300% djap.png.cropped.png djap.png.cropped.300.png
 tesseract djap.png.cropped.300.png  jap_ocr -l jpn >/dev/null 2>&1
 #tesseract djap.png.cropped.300.png  jap_ocr -l jpn
 #THE_WORDS=$(< jap_ocr.txt)
+TIME1=$(date)
 THE_WORDS_READ=$(python3 read_jap.py)
 THE_WORDS_READ_SCORE=$(python3 do_translate_and_score.py $THE_WORDS_READ)
 THE_WORDS_READ_ENGLISH=$(python3 do_translate.py $THE_WORDS_READ)
+
+THE_FILE=$CONVERSATION_FILE.log
+echo -n $BOT_NAME_ELENA >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $TIME1 >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $THE_WORDS_READ_SCORE >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo $THE_WORDS_READ >> $THE_FILE
+
+
+THE_FILE=$CONVERSATION_FILE_ENGLISH.log
+echo -n $BOT_NAME_ELENA >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $TIME1 >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $THE_WORDS_READ_SCORE >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo $THE_WORDS_READ_ENGLISH >> $THE_FILE
+
+
+
 THE_WORDS_REPLY=$(python3 do_response.py $THE_WORDS_READ)
 THE_WORDS_REPLY_SCORE=$(python3 do_translate_and_score.py $THE_WORDS_REPLY)
 THE_WORDS_REPLY_ENGLISH=$(python3 do_translate.py $THE_WORDS_REPLY)
+
+
+TIME1=$(date)
+THE_FILE=$CONVERSATION_FILE.log
+echo -n $BOT_NAME_DAN >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $TIME1 >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $THE_WORDS_REPLY_SCORE >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo $THE_WORDS_REPLY >> $THE_FILE
+
+
+THE_FILE=$CONVERSATION_FILE_ENGLISH.log
+echo -n $BOT_NAME_DAN >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $TIME1 >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo -n $THE_WORDS_REPLY_SCORE >> $THE_FILE
+echo -n ' : ' >> $THE_FILE
+echo $THE_WORDS_REPLY_ENGLISH >> $THE_FILE
+
+
+
 echo ' '
 echo '----------'
 echo '---Read---'
--- linux/run.sh
+++ linux/run.sh
@@ -3,5 +3,5 @@ while [ 1 -eq 1 ]
 do
 	python3 do_move_mouse.py
 	./do_screen_cap_and_ocr.sh
-	sleep 15
+	sleep 60
 done

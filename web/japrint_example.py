b691ae7d0b9cf12850f89fe77169581486155909
Daniel M. Lofaro <dan@danlofaro.com>
2022-06-19 02:47:13 +0000

    trying to make ja print to html work

---
+++ web/japrint_example.py
@@ -0,0 +1,16 @@
+from jsConsole import *
+#### And now you can use classes like document, window, Math or console from Python!
+
+console.log('jsConsole is ready!')
+'jsConsole is ready!'
+
+window.open('https://google.com', '_self')
+# Opens google.com on the browser #
+
+def hello():
+    print('Hello')
+
+
+    document.getElementsByClassName('RNNXgb')[0].addEventListener('click', hello) ### Adding an event listener to the search bar from google.com which executes hello() when clicked.
+
+    window.kill() # Needed to kill the browser (for it not to stay in the background even with Python quited)
